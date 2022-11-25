e0969d3025e988928fcc09a186fc624355ce5b31
Jesper Nielsen <jesper.nielsen_98@hotmail.com>
2022-11-25 14:23:43 +0100

    added base idea of how to make the hover info box

--- script.js
+++ script.js
@@ -4,13 +4,6 @@ window.addEventListener("load", () => {
   getAll().then((apiBooks) => (bookList = apiBooks));  
 });
 searchField.addEventListener('keyup', (e) => searchBooks(e.target.value));
-/* searchField.addEventListener("keyup", (e) => renderBookList(
-    bookList.filter(({ title, author }) => {
-        const searchTerm = e.target.value.toLowerCase();
-        return title.toLowerCase().indexOf(searchTerm) >= 0 || author.toLowerCase().indexOf(searchTerm) >= 0;
-    })
-  )
-); */
 async function searchBooks(searchTerm) {
     renderBookList(
         bookList.filter(
@@ -29,12 +22,15 @@ function renderBookList(bookList) {
 
     bookList.length > 0 && searchField.value && root.insertAdjacentHTML('beforeend', BookList(bookList));
 }
+/* function renderBookDetail(bookDetail) {
+    
+} */
 /* const bookHover {
     let html = `
-    <ul class="box-border h-[17rem] w-64 border-4 rounded-md border-black bg-neutral-700 list-inside">
+    <ul id="bookDetail" class="box-border h-[17rem] w-64 border-4 rounded-md border-black bg-neutral-700 list-inside">
         <li class="text-2xl font-semibold m-1">Title: Oliver Twist</li>
         <li class="m-2">Author: Charles Dickens</li>
-        <img class="float-right max-w-[70%] m-2" src="https://www.empowervate.org/wp-content/uploads/2015/11/circle.jpg" alt="colored cirles">
+        <img class="float-right max-w-[70%] m-2" src="https://www.empowervate.org/wp-content/uploads/2015/11/circle.jpg" alt="book cover">
         <p class="m-2">Cover: </p>
     </ul>`;
     return html;
